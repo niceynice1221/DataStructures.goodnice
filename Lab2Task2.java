5323f2c519a916313b9224a7adae2462fbebbb76
Good-nice <67329264+niceynice1221@users.noreply.github.com>
2022-06-28 13:24:10 +0700

    Update Lab2Task2.java

--- Lab2Task2.java
+++ Lab2Task2.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 public class Lab2Task2 {
 public static void main(String[] args) {
         int x[] = {10,12,50,45,98,32,2};
-        System.out.println(Arrays.sort(x));
+         Arrays.sort(x);
         System.out.println(printArr(x));
 }
 static String printArr(int[] arr){
