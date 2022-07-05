f3da2a33980ba88a35a534b8ff5138334cff0c41
Good-nice <67329264+niceynice1221@users.noreply.github.com>
2022-07-05 13:11:34 +0700

    Add files via upload

---
+++ Lab3Task1.java
@@ -0,0 +1,18 @@
+public class Lab3Task1 {
+    public static void main(String[] args) {
+    int n = 5;
+    for (int i=1; i<n; i++){
+    for (int j=i+1; j<=n; j++){
+    for (int k=j+1; k<=n; k++){
+    
+    System.out.print(i+","+j+","+k+" ");
+    
+    }
+    
+    System.out.println();
+    }
+    System.out.println();
+    }
+    System.out.println();
+    }
+    }
---
+++ Lab3Task2.java
@@ -0,0 +1,14 @@
+public class Lab3Task2 {
+   
+public static void main(String[] args) {
+int n = 1000;
+int i, j, m = 0;
+for (i=0 ; i<n ; i++ ){
+for(j=0 ; j<n ; j++ ){
+m = m +(i*j);
+}
+}
+System.out.println(m);
+}
+}
+
