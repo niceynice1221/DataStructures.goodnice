8070d7211830757daf5c07c648bcea818f0ab965
Good-nicxe:) <67329264+niceynice1221@users.noreply.github.com>
2022-09-13 13:59:48 +0700

    Add files via upload

---
+++ BTNode.java
@@ -0,0 +1,16 @@
+public class BTNode {
+    public BTNode left, right;
+    public int data;
+
+    public BTNode() { // constructor
+        left = null;
+        right = null;
+        data = 0;
+    }
+
+    public BTNode(int data) { // constructor
+        left = null;
+        right = null;
+        this.data = data;
+    }
+}
